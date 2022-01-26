7b2940d0b138f6c06d5dc4d4bb355f5f478639ce
ahmad-swedani <swedani390@gmail.com>
2022-01-26 12:04:52 +0200

    automaterobot: get the new challenge and test with one command

--- bin/pull-challenge.sh
+++ bin/pull-challenge.sh
@@ -2,5 +2,5 @@
 
 echo "pulling Code Challenge $1"
 
-`which curl` -s https://gg-devs.github.io/problem-space/day-$1/Challenge$1.js > ./code-challenges/challenges-$1.js
-`which curl` -s https://gg-devs.github.io/problem-space/day-$1/Challenge$1.test.js > ./test/challenges-$1.test.js
+`which curl` -s https://ltuc.github.io/prep-challenges-content/day-$1/Challenge$1.js > ./code-challenges/challenges-$1.js
+`which curl` -s https://ltuc.github.io/prep-challenges-content/day-$1/Challenge$1.test.js > ./test/challenges-$1.test.js
