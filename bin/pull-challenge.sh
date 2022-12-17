f6f64fa6a374e269e7f9b7142fbb665a6a66e1ea
Boshra Jaber <boshrajaber94@gmail.com>
2022-12-17 23:05:17 +0300

    Update pull-challenge.sh

--- bin/pull-challenge.sh
+++ bin/pull-challenge.sh
@@ -5,10 +5,10 @@ echo "pulling Code Challenge $1"
 if [ $1 == '10'  ] || [ $1 == '15'  ] || [ $1 == '19'  ]
 then
     mkdir code-challenges/challenges-$1
-    `which curl` -s https://ltuc.github.io/prep-challenges-content/day-$1/Challenge$1.nnb > ./code-challenges/challenges-$1/challenges-$1.nnb
-    `which curl` -s https://ltuc.github.io/prep-challenges-content/day-$1/README.md > ./code-challenges/challenges-$1/README.md
+    `which curl` -s https://ltuc.github.io/prep-challenges-content/Code-Challenge-$1/Challenge$1.nnb > ./code-challenges/challenges-$1/challenges-$1.nnb
+    `which curl` -s https://ltuc.github.io/prep-challenges-content/Code-Challenge-$1/README.md > ./code-challenges/challenges-$1/README.md
 
 else
-    `which curl` -s https://ltuc.github.io/prep-challenges-content/day-$1/Challenge$1.js > ./code-challenges/challenges-$1.js
-    `which curl` -s https://ltuc.github.io/prep-challenges-content/day-$1/Challenge$1.test.js > ./test/challenges-$1.test.js
+    `which curl` -s https://ltuc.github.io/prep-challenges-content/Code-Challenge-$1/Challenge$1.js > ./code-challenges/challenges-$1.js
+    `which curl` -s https://ltuc.github.io/prep-challenges-content/Code-Challenge-$1/Challenge$1.test.js > ./test/challenges-$1.test.js
 fi
